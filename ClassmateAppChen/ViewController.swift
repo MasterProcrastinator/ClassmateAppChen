cc8263ea679791fc5010d28378a2c44106316196
Alvin <thisiseasytoremember1224@gmail.com>
2023-10-11 09:15:07 -0500

    Initial Commit

---
+++ ClassmateAppChen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassmateAppChen
+//
+//  Created by ALVIN CHEN on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
