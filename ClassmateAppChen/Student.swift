c3aacf7214b4a8d89a5dc5d7ad0474233b848851
Alvin <thisiseasytoremember1224@gmail.com>
2023-10-12 09:49:32 -0500

    asd

---
+++ ClassmateAppChen/Student.swift
@@ -0,0 +1,24 @@
+//
+//  Student.swift
+//  ClassmateAppChen
+//
+//  Created by ALVIN CHEN on 10/12/23.
+//
+
+import Foundation
+public class Student{
+    
+    var name : String
+    var age : Int
+    var rating : String
+    
+    init(name: String, age: Int, rating: String){
+        self.name = name
+        self.age = age
+        self.rating = rating
+    }
+    
+    
+    
+    
+}
